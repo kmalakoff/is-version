a95a480bd7147be0f121948e927a4fcd660d819a
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-21 07:11:54 -0700

    Added optional prefix

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var isNaN = require('./lib/isNaN');
 
-module.exports = function isVersion(version) {
-  if (version[0] !== 'v') return false;
-  var parts = version.slice(1).split('.');
+module.exports = function isVersion(version, prefix) {
+  if (typeof version !== 'string') return false;
+
+  if (prefix) {
+    if (typeof prefix !== 'string') prefix = '' + prefix;
+    if (version.indexOf(prefix) !== 0) return false;
+    version = version.slice(prefix.length);
+  }
+  var parts = version.split('.');
   if (parts.length !== 3) return false;
   return !isNaN(+parts[0]) && !isNaN(+parts[1]) && !isNaN(+parts[2]);
 };
